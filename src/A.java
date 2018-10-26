c8e75b151be99607edce04a1327da166b3ec5675
JFourier <934087288@qq.com>
2018-10-26 10:59:44 +0800

    idea backup

--- src/A.java
+++ src/A.java
@@ -8,7 +8,7 @@ public class A {
     public static void main(String[] args){
         int a=10;
         int b=a;
-        a=a+1;
+        a=a+2;
         System.out.println(a);
         System.out.println(b);
     }
