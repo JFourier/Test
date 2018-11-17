342027e5a375772607520d68b97eadbb08255352
JFourier <934087288@qq.com>
2018-11-17 14:14:21 +0800

    idea backup

---
+++ src/testforJDK11.java
@@ -0,0 +1,12 @@
+/**
+ * 〈〉
+ *
+ * @author He.H
+ * @create 2018/11/14
+ */
+public class testforJDK11 {
+    public static void main(String[] args){
+        var a ="jhdsgfjwge";
+        System.out.println(a);
+    }
+}
