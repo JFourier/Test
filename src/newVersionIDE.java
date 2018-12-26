972001698230e33e7fcf59c3d01de20e806b3000
JFourier <934087288@qq.com>
2018-12-26 14:32:31 +0800

    idea backup

--- src/newVersionIDE.java
+++ src/newVersionIDE.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class newVersionIDE {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt();
-//        int t = scanner.nextInt();
-        String i = scanner.nextLine();
-        String[] h = i.split("");
+        String s = "34thjahejt";
+        char[] arr = s.toCharArray();
+        System.out.println(arr[0]);
     }
 }
