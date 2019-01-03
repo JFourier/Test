f294e53eb22933d6849db6df42795b2480697d55
JFourier <934087288@qq.com>
2019-01-03 21:20:46 +0800

    idea backup

--- src/TestForMap.java
+++ src/TestForMap.java
@@ -10,15 +10,8 @@ import java.util.HashMap;
  */
 public class TestForMap {
     public static void main(String[] args){
-//        int sum = 0;
-//        for (int i = 0; i<=10 ; i++){
-//            sum += i;
-//        }
-//        System.out.println(sum);
-        HashMap<Integer, Integer> imap = new HashMap<>();
-        imap.put(1,2);
-        imap.put(2,3);
-        int a = imap.get(1);
-        int b = imap.getOrDefault(3,9);
+
+        int[][] a = {{1,2,3},{4,5,7,9,5}};
+
     }
 }
